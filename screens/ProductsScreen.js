d226e4a06b674eaa8e89e002df7c72876fd49193
Alok Singh <singhalok641@gmail.com>
2018-03-29 10:34:19 +0530

    cart screen offers tab

--- screens/ProductsScreen.js
+++ screens/ProductsScreen.js
@@ -4,14 +4,15 @@ import {
   ScrollView,
   StyleSheet,
   View,
-  TouchableHighlight } from 'react-native'
+  TouchableHighlight,
+  ActivityIndicator } from 'react-native'
 import {
   Container,
   Text,
   List,
   ListItem } from 'native-base'
 
-const image = require('../../assets/images/whis.jpg')
+const image = require('../assets/images/whis.jpg')
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -55,16 +56,54 @@ const styles = StyleSheet.create({
 })
 
 export default class OrdersScreen extends React.Component {
-  static navigationOptions = {
-    title: 'Women Care'
-  }
+  static navigationOptions = ({ navigation }) => ({
+    title: `${navigation.state.params.title}`,
+  })
 
   constructor(props) {
     super(props)
+    this.state = {
+      isLoading: true,
+      products: {},
+      response: {}
+    }
+  }
+
+  componentDidMount = async () => {
+    fetch(`http://192.168.43.217:8082/stores/users/getProducts/${this.props.navigation.state.params.category}`,
+    {
+       method: 'GET',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+          'Host': '192.168.43.217:8082'
+        }
+      })
+      .then((response) => response.json())
+      .then((responseJson) => {
+        this.setState({
+          isLoading: false,
+          response: responseJson,
+          products: responseJson.products 
+        }, function() {
+          console.log(this.state.products)
+        });
+      })
+      .catch((error) => {
+        console.error(error);
+      });
   }
 
   render() {
     const { navigate } = this.props.navigation
+    if (this.state.isLoading) {
+      return (
+        <View style={{flex: 1, paddingTop: 20}}>
+          <ActivityIndicator />
+        </View>
+      );
+    }
+
     return (
       <Container>
         <View style={styles.container}>
@@ -72,17 +111,19 @@ export default class OrdersScreen extends React.Component {
             style={styles.container}
             contentContainerStyle={styles.contentContainer} showsVerticalScrollIndicator={false}>
             <View>
-              <List>
+              <List 
+                dataArray={this.state.products}
+                renderRow={(product) =>
                 <ListItem>
                   <View style={styles.view}>
-                    <Image resizeMode = 'contain' style={styles.image} source={image} />
+                    <Image resizeMode = 'contain' style={styles.image} source={{uri:product.imagePath}} />
                     <View style={ styles.info }>
                       <View style={{ justifyContent: 'flex-start', paddingTop: 3 }}>
-                        <Text style={styles.pro_name}>Whisper Ultra Nights Wings Sanitary Pads Pack of 2</Text>
+                        <Text style={styles.pro_name}>{product.name}</Text>
                       </View>
-                      <Text note style={styles.descrip}>packet of 5 pads</Text>
+                      <Text note style={styles.descrip}>{product.brand}</Text>
                       <View style={{ justifyContent: 'center', alignItems: 'center', flexDirection: 'row', paddingLeft: 5, paddingTop: 5 }}>
-                        <Text style={{ flex: 1, fontSize: 14, color: '#4d4d4d', alignSelf: 'flex-end', paddingBottom: 2 }}>₹ 300</Text>
+                        <Text style={{ flex: 1, fontSize: 14, color: '#4d4d4d', alignSelf: 'flex-end', paddingBottom: 2 }}>{product.price}</Text>
                         <TouchableHighlight onPress={() => navigate('EverydayScreen')} underlayColor='#dbdbdb'>
                           <View style={styles.button}>
                             <Text style={{ fontSize: 14, color: '#ffffff', fontWeight: 'bold' }}>ADD TO CART</Text>
@@ -92,6 +133,7 @@ export default class OrdersScreen extends React.Component {
                     </View>
                   </View>
                 </ListItem>
+              }>
               </List>
             </View>
           </ScrollView>
